e2a858b3f184fd3200064bdda85bf9e79f1cdcc5
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-07-23 13:24:01 +0800

    Completed all procedures

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
+console.log('\'' +name.toUpperCase(0) + '\'');
+
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+var whole = "";
+var myWord = sentence.split(" ");
+for(let i = 0; i < myWord.length; i++){
+myWord[i] = myWord[i][0].toUpperCase() + myWord[i].substr(1);
+whole += myWord[i]+= ' ';
+}
+console.log(myWord);
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
+console.log(money.substr(1));
